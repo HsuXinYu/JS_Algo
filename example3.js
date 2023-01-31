237c5ea9f88549f5af0886a33fdd0bcfd5f8c924
xinyu <jolin7180732@gmail.com>
2023-01-31 10:29:34 +0800

    class 3

---
+++ example3.js
@@ -0,0 +1,27 @@
+function example(n) {
+  let counter = 0;
+  for (let i = 0; i < 3 * n; i++) {
+    // console.log("Hello");
+    counter ++;
+  }
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      // console.log("Hello");
+      counter ++;
+    }
+  }
+
+  // console.log("Hello");
+  // console.log("Hello");
+  // console.log("Hello");
+  // console.log("Hello");
+  counter +=4;
+  
+  return counter;
+}
+
+// example(2);
+for (let i = 2; i < 10; i++){
+  console.log(example(i));
+}
