f002f09736ac496c83aa581f1b28f0a2bb888922
xinyu <jolin7180732@gmail.com>
2023-02-08 14:10:40 +0800

    class 4

--- intersection.js
+++ intersection.js
@@ -27,3 +27,27 @@ function intersection(arr1, arr2) {
 }
 
 intersection([15, 3, 6, 8, 24, 16], [11, 3, 9, 6, 15, 8]);
+
+//counter
+function counter(arr1, arr2) {
+  let result = [];
+  let arr3 = arr1.concat(arr2);
+  let counter = {};
+
+  for (i = 0; i < arr3.length; i++) {
+    if (!counter[arr3[i]]) {
+      counter[arr3[i]] = 1;
+    } else {
+      counter[arr3[i]]++;
+    }
+  }
+  for (let property in counter) {
+    if (counter[property] >= 2) {
+      result.push(property);
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+counter([15, 3, 6, 8, 24, 16], [11, 3, 9, 6, 15, 8]);
