f954952ee4fd4d512924a5ec6b2d37f2c6ca5a82
xinyu <jolin7180732@gmail.com>
2023-02-24 21:39:01 +0800

    class 4

--- 16.insertionSort.js
+++ 16.insertionSort.js
@@ -6,7 +6,7 @@ function insertionSort(arr) {
     while (j >= 0 && arr[j] > key) {
       //當j不是最小值且j大於key時執行
       arr[j + 1] = arr[j];
-      j -= 1;
+      j--;
     }
     arr[j + 1] = key;
   }
@@ -17,3 +17,17 @@ function insertionSort(arr) {
 let test = [14, -4, 17, 6, 22, 1, -5];
 
 insertionSort(test);
+
+// function insertionSort(arr) {
+//   console.log(arr);
+//   for (i = 0; i <= arr.length - 1; i++) {
+//     let key = arr[i];
+//     let j = i - 1;
+//     for (; j >= 0 && arr[j] > key; j--) {
+//       arr[j + 1] = arr[j];
+//     }
+//     arr[j + 1] = key;
+//   }
+//   console.log(arr);
+//   return arr;
+// }
