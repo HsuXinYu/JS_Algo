a03e174236437e87acb852c2011a9fdfe6d8f259
xinyu <jolin7180732@gmail.com>
2023-03-02 16:14:00 +0800

    class 6

--- 19.heapSort.js
+++ 19.heapSort.js
@@ -6,15 +6,15 @@ function buildMaxHeap() {
   heapsize = arr.length - 1;
   for (let i = Math.floor(heapsize / 2); i >= 0; i--) {
     maxHeapify(i);
-    // console.log(i);
   }
 }
+
 function maxHeapify(i) {
   let largest;
   let l = i * 2 + 1;
   let r = i * 2 + 2;
   if (l <= heapsize && arr[l] > arr[i]) {
-    //假如左孩子不是在樹狀最後一個位置且值大於父親
+    //值大於父親
     largest = l;
   } else {
     largest = i;
@@ -30,18 +30,22 @@ function maxHeapify(i) {
     arr[largest] = temp;
     maxHeapify(largest);
   }
+  // console.log(i, arr);
 }
+
 function heapSort() {
   buildMaxHeap();
-  //將已排好的二元數最大值與最後一個值交換後陣列長度-1，在執行一次maxHeapify
-  for (i = arr.langth - 1; i >= 0; i--) {
+  //將已排好的二元樹與最後一個值交換後，整個陣列長度-1，在執行一次maxHeapify
+  for (let i = arr.langth - 1; i >= 0; i--) {
     let temp = arr[0];
     arr[0] = arr[i];
     arr[i] = temp;
     heapsize -= 1;
     maxHeapify(0);
+    console.log(i);
   }
   return arr;
 }
 
-console.log(heapSort());
+heapSort();
+console.log(arr);
---
+++ 20.quickSort.js
@@ -0,0 +1,29 @@
+let arr = [15, 3, 17, 18, 20, 1, 66];
+
+function partition(p, r) {
+  let x = arr[r]; //pivot
+  let i = p - 1;
+  for (j = p; j <= r - 1; j++) {
+    if (arr[j] <= x) {
+      i++;
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+  let temp = arr[i + 1];
+  arr[i + 1] = arr[r];
+  arr[r] = temp;
+  return i + 1;
+}
+
+function quickSort(p, r) {
+  if (p < r) {
+    let q = partition(p, r);
+    quickSort(p, q - 1);
+    quickSort(q + 1, r);
+  }
+}
+
+quickSort(0, arr.length - 1);
+console.log(arr);
