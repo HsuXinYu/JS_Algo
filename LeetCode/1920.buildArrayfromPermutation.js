f26a1295e1362c47e1a6274eebee708033ab9fb1
xinyu <jolin7180732@gmail.com>
2023-03-07 19:36:49 +0800

    20230307

---
+++ LeetCode/1920.buildArrayfromPermutation.js
@@ -0,0 +1,12 @@
+function BuildArrayfromPermutation(nums) {
+  let number = [];
+  for (i = 0; i <= nums.length - 1; i++) {
+    number[i] = nums[nums[i]];
+  }
+  console.log(number);
+  return number;
+}
+
+let nums = [0, 2, 1, 5, 3, 4]; //[0,1,2,4,5,3]
+
+BuildArrayfromPermutation(nums);
---
+++ LeetCode/1929.concatenationOfArray.js
@@ -0,0 +1,19 @@
+function ConcatenationOfArray(nums) {
+  let numbers = [];
+  let i = 0;
+  let len = nums.length * 2;
+  while (i < len) {
+    for (j = 0; j < nums.length; j++) {
+      numbers[i] = nums[j];
+      i++;
+    }
+  }
+  console.log(numbers);
+  return numbers;
+}
+
+let nums = [1, 2, 1];
+
+ConcatenationOfArray(nums);
+
+// ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
