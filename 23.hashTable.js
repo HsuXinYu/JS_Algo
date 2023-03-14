d540d2c32b49effdad8778c43cd67f1df6b77d6a
xinyu <jolin7180732@gmail.com>
2023-03-14 08:32:17 +0800

    class 9

--- 23.hashTable.js
+++ 23.hashTable.js
@@ -7,6 +7,14 @@ class HashTable {
     }
   }
 
+  parse(str) {
+    let result = 0;
+    for (let i = 0; i < str.length; i++) {
+      result += str.charCodeAt(i);
+    }
+    return result % this.size;
+  }
+
   //division method
   hash1(key) {
     return key % this.size;
@@ -14,8 +22,14 @@ class HashTable {
 
   //multiplication
   hash2(key) {
+    let parsedKey = 0;
+    if (typeof key !== "number") {
+      parsedKey = this.parse(key);
+    } else {
+      parsedKey = key;
+    }
     let A = (Math.sqrt(5) - 1) / 2;
-    return Math.floor(this.size * ((key * A) % 1));
+    return Math.floor(this.size * ((parsedKey * A) % 1));
   }
 
   set(key, value) {
@@ -40,11 +54,25 @@ class HashTable {
 
 let myHashTable = new HashTable(6);
 
-myHashTable.set(11424, "mike");
-myHashTable.set(6254, "james");
-myHashTable.set(4171, "drake");
-myHashTable.set(554, "kevin");
+// myHashTable.set(11424, "mike");
+// myHashTable.set(6254, "james");
+// myHashTable.set(4171, "drake");
+// myHashTable.set(554, "kevin");
 
+// console.log(myHashTable.get(11424));
+
+myHashTable.set("white", "#FFFFFF");
+myHashTable.set("magenta", "#FF00FF");
+myHashTable.set("red", "#FF0000");
+
+// console.log(myHashTable.get("red").value);
 // myHashTable.printAll();
 
-console.log(myHashTable.get(11424));
+//object and arrays are hashed
+let Harry = {
+  fullname: "Harry Potter",
+  age: "15",
+  school: "Hogwarts",
+};
+
+console.log(Harry.fullname);
