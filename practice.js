1413f543feec7fa3278112bdd40b8fa3020d74ef
xinyu <jolin7180732@gmail.com>
2023-03-14 18:00:55 +0800

    20230314

--- practice.js
+++ practice.js
@@ -1,47 +1,16 @@
-function practice(arr) {
-  for (i = 0; i < arr.length - 1; i++) {
-    for (j = 0; j < arr.length - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
+function bubble(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[i] < arr[j]) {
         let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+        arr[j] = arr[i];
+        arr[i] = temp;
       }
     }
-  }
-  console.log(arr);
-  return arr;
-}
-
-function practice1(arr) {
-  for (i = 0; i <= arr.length - 1; i++) {
-    let key = arr[i];
-    let j = i - 1;
-    console.log(arr);
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = key;
-    console.log(arr);
-  }
-  return arr;
-}
-
-function practice2(arr) {
-  for (i = 0; i < arr.length - 1; i++) {
-    let minIndex = i;
-    for (j = i; j <= arr.length - 1; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
-    let temp = arr[minIndex];
-    arr[minIndex] = arr[i];
-    arr[i] = temp;
   }
   console.log(arr);
   return arr;
 }
 
 let test = [14, -4, 17, 6, 22, 1, -5];
-practice1(test);
+bubble(test);
